254ef1543940ba540fe11fd6d0b8676a8440bec1
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2024-06-14 16:14:53 +0200

    Create helper class to work with led/buzzer impulse

---
+++ src/pulse.h
@@ -0,0 +1,41 @@
+#ifndef __PULSE_H__
+#define __PULSE_H__
+
+#define PULSE_STOP 0
+#define PULSE_100MS 1
+#define PULSE_1000MS 2
+
+class Pulse {
+public:
+  Pulse(uint8_t pin) {
+    _pin = pin;
+  }
+
+  void tick(void) {
+    if (_delay != 0 && millis() - _timestamp >= _delay) {
+      _state = !_state;
+      _timestamp = millis();
+    }
+    digitalWrite(_pin, _state);
+  }
+
+  void set(uint8_t mode) {
+    _timestamp = millis();
+    if (mode == PULSE_STOP) {
+      _state = false;
+      _delay = 0;
+    } else if (mode == PULSE_100MS) {
+      _delay = 100;
+    } else if (mode == PULSE_1000MS) {
+      _delay = 1000;
+    }
+  }
+
+private:
+  uint8_t _pin = 0;
+  bool _state = false;
+  uint32_t _delay = 0;
+  uint32_t _timestamp = 0;
+};
+
+#endif
